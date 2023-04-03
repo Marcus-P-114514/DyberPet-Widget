6bfde650406a153457504c6d8a8082f1da22a107
Marcus <102861175+Marcus-P-114514@users.noreply.github.com>
2023-04-03 19:22:03 +0800

    💻 Updated: Status monitor - user interface

---
+++ src/extras/status_monitor/StatusMonitor.cpp
@@ -0,0 +1,13 @@
+#include "StatusMonitor.h"
+
+StatusMonitor::StatusMonitor(QWidget *parent)
+	: QWidget(parent)
+	, ui(new Ui::StatusMonitorClass())
+{
+	ui->setupUi(this);
+}
+
+StatusMonitor::~StatusMonitor()
+{
+	delete ui;
+}
---
+++ src/extras/status_monitor/StatusMonitor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QWidget>
+#include "ui_StatusMonitor.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class StatusMonitorClass; };
+QT_END_NAMESPACE
+
+class StatusMonitor : public QWidget
+{
+	Q_OBJECT
+
+public:
+	StatusMonitor(QWidget *parent = nullptr);
+	~StatusMonitor();
+
+private:
+	Ui::StatusMonitorClass *ui;
+};
