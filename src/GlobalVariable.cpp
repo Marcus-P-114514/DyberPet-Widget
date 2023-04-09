fca1555556b1e82ec7eecfaf590f7da8c58d7b2b
Marcus <102861175+Marcus-P-114514@users.noreply.github.com>
2023-04-09 15:19:36 +0800

    📄 Updated: Base dir

--- src/GlobalVariable.cpp
+++ src/GlobalVariable.cpp
@@ -2,20 +2,30 @@
 
 #ifdef Q_OS_WIN
 QString GlobalVariable::CurrentPlatform = "Windows";
+QString GlobalVariable::BaseDir = "./";
+QString GlobalVariable::ConfigPath = GlobalVariable::BaseDir + "config/config.json";
 #endif // Win32
 
 #ifdef Q_OS_MACOS
 QString GlobalVariable::CurrentPlatform = "macOS";
+QString GlobalVariable::BaseDir = QString DataDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+QString GlobalVariable::ConfigPath = GlobalVariable::BaseDir + "/config/config.json";
 #endif // macOS
 
 #ifdef Q_OS_LINUX
 QString GlobalVariable::CurrentPlatform = "Linux";
+QString GlobalVariable::BaseDir = QString DataDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+QString GlobalVariable::ConfigPath = GlobalVariable::BaseDir + "/config/config.json";
 # endif // Linux
 
 #ifdef Q_OS_ANDROID
 QString GlobalVariable::CurrentPlatform = "Android";
+QString GlobalVariable::BaseDir = QString DataDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+QString GlobalVariable::ConfigPath = GlobalVariable::BaseDir + "/config/config.json";
 #endif // Android
 
 #ifdef Q_OS_IOS
 QString GlobalVariable::CurrentPlatform = "iOS";
+QString GlobalVariable::BaseDir = QString DataDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+QString GlobalVariable::ConfigPath = GlobalVariable::BaseDir + "/config/config.json";
 #endif // iOS
